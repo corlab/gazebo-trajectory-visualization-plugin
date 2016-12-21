6653ca22b892c129cdae6e94a5cf9bb562795486
Julian Seidelmann <jseidelmann@techfak.uni-bielefeld.de>
2016-12-21 13:33:26 +0100

    added missing methods for pointAdd and lifecycle

--- src/orocos/orocosTrajectoryService.cpp
+++ src/orocos/orocosTrajectoryService.cpp
@@ -2,6 +2,8 @@
 #include <ignition/msgs.hh>
 #include <ignition/transport.hh>
 
+#include <rtt/plugin/ServicePlugin.hpp>
+#include <rtt/base/ExecutableInterface.hpp>
 #include <rtt/RTT.hpp>
 #include <rtt/Component.hpp>
 
@@ -13,7 +15,7 @@ using namespace Orocos;
 namespace TrajectoryService
 {
     class GazeboCommunication
-        : public TaskContext
+        : public RTT::Service
     {
 
 	// Create a transport node.
@@ -22,9 +24,10 @@ namespace TrajectoryService
  	// Prepare the input parameters.
 	ignition::msgs::StringMsg req;
     public:
+        
 
-        GazeboCommunication(std::string name)
-            : RTT::TaskContext(name)
+        GazeboCommunication(TaskContext* owner)
+            : RTT::Service("TrajectoryService", owner)
         {
 		this->addOperation("newTrajectoryModel", &GazeboCommunication::newTrajectoryModel, this, RTT::OwnThread).doc("Adds trajectory of the specific model.")
 			.arg("model", "Name of the model");
@@ -56,11 +59,36 @@ namespace TrajectoryService
 			.arg("model", "Name of the model")
 			.arg("link","Name of the link");
 
+		this->addOperation("newCustomTrajectory", &GazeboCommunication::newCustomTrajectory, this, RTT::OwnThread).doc(".")
+			.arg("model", "Name of the model");
+
+		this->addOperation("addPoint", &GazeboCommunication::addPoint, this, RTT::OwnThread).doc(".")
+			.arg("model", "Name of the model")
+			.arg("x", "")
+			.arg("y", "")
+			.arg("z", "");
+
+		this->addOperation("setLifecycleModel", &GazeboCommunication::setLifecycleModel, this, RTT::OwnThread).doc("Resumes drawing of a specific trajectory.")
+			.arg("model", "Name of the model");
+		this->addOperation("setLifecycleLink", &GazeboCommunication::setLifecycleLink, this, RTT::OwnThread).doc("Resumes drawing of a specific trajectory.")
+			.arg("model", "Name of the model")
+			.arg("link","Name of the link");
+
+		this->addOperation("removeLifecycleModel", &GazeboCommunication::removeLifecycleModel, this, RTT::OwnThread).doc("Resumes drawing of a specific trajectory.")
+			.arg("model", "Name of the model");
+		this->addOperation("removeLifecycleLink", &GazeboCommunication::removeLifecycleLink, this, RTT::OwnThread).doc("Resumes drawing of a specific trajectory.")
+			.arg("model", "Name of the model")
+			.arg("link","Name of the link");
+		
+
+
 		this->addOperation("clear", &GazeboCommunication::clear, this, RTT::OwnThread).doc("Clears all trajectorys.");
 		this->addOperation("delAll", &GazeboCommunication::delAll, this, RTT::OwnThread).doc("Deletes all trajectorys.");
 		this->addOperation("pause", &GazeboCommunication::pause, this, RTT::OwnThread).doc("Pauses drawing of all trajectorys");
 		this->addOperation("resume", &GazeboCommunication::resume, this, RTT::OwnThread).doc("Resumes drawing of all trajectorys.");
-        }
+		
+		this->doc("TrajectoryService service interface.");     
+	}
 
 	void newTrajectoryModel(std::string model)
 	{
@@ -163,32 +191,49 @@ namespace TrajectoryService
 		req.set_data("HELLO");
 		bool executed = node.Request("/trajectory/command/resume", req);
 		if (!executed) std::cerr << "Service call failed" << std::endl;
-
 	}
 
-        void updateHook()
-        {
-
-        }
-        bool configureHook()
-        {
-
-
-        	return true;
-        }
-	bool startHook(){
-
-		return true;
+	void setLifecycleModel(std::string model)
+	{
+		req.set_data(model);
+		bool executed = node.Request("/trajectory/command/setLifecycle", req);
+		if (!executed) std::cerr << "Service call failed" << std::endl;
 	}
-	void stopHook(){
 
-		
+	void setLifecycleLink(std::string model,std::string link)
+	{
+		req.set_data(model+"::"+link);
+		bool executed = node.Request("/trajectory/command/setLifecycle", req);
+		if (!executed) std::cerr << "Service call failed" << std::endl;	
 	}
-	void cleanupHook(){
-		
-		
+
+	void removeLifecycleModel(std::string model)
+	{
+		req.set_data(model);
+		bool executed = node.Request("/trajectory/command/removeLifecycle", req);
+		if (!executed) std::cerr << "Service call failed" << std::endl;
+	}
+
+	void removeLifecycleLink(std::string model,std::string link)
+	{
+		req.set_data(model+"::"+link);
+		bool executed = node.Request("/trajectory/command/removeLifecycle", req);
+		if (!executed) std::cerr << "Service call failed" << std::endl;	
+	}
+
+	void newCustomTrajectory(std::string model)
+	{
+		req.set_data(model);
+		bool executed = node.Request("/trajectory/command/newCustomTrajectory", req);
+		if (!executed) std::cerr << "Service call failed" << std::endl;
+	}
+
+	void addPoint(std::string model,std::string x,std::string y,std::string z)
+	{
+		req.set_data(model+" "+x+" "+y+" "+z);
+		bool executed = node.Request("/trajectory/command/addPoint", req);
+		if (!executed) std::cerr << "Service call failed" << std::endl;
 	}
     };
 }
-
-ORO_CREATE_COMPONENT( TrajectoryService::GazeboCommunication )
+ORO_SERVICE_NAMED_PLUGIN(TrajectoryService::GazeboCommunication, "dot")
